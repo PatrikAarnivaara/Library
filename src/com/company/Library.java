ce6a7dfb954b2e6eac18798a3b18f9b03b28d1b5
Patrik Aarnivaara <patrikaarnivaara@gmail.com>
2020-01-14 09:42:48 +0100

    Started working on switch menus

--- src/com/company/Library.java
+++ src/com/company/Library.java
@@ -1,9 +1,12 @@
 package com.company;
 
+import java.sql.SQLOutput;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Library {
 
+    Scanner scanner = new Scanner(System.in);
     private ArrayList<Book> books = new ArrayList<>();
     private ArrayList<Person> borrowers = new ArrayList<>();
 
@@ -30,12 +33,96 @@ public class Library {
     }
 
     public void start() {
-        showAllBooks();
+
+        while (true) {
+
+            System.out.println("--- Main menu ---");
+            System.out.println("1. Borrower");
+            System.out.println("2. Administrator");
+            System.out.println("3. Exit");
+            System.out.println("-----------------");
+            int option = Integer.parseInt(scanner.nextLine());
+
+            //if to control input
+
+            switch (option) {
+                case 1:
+                    //Verify user here
+                    borrowerMenu();
+                    break;
+                case 2:
+                    //Verify admin here
+                    //adminMenu();
+                    break;
+                case 3:
+                    System.exit(0);
+                    break;
+                default:
+                    System.out.println("Enter a number between 1-3.");
+                    break;
+
+            }
+        }
+        //showAllBooks();
+    }
+
+    private void borrowerMenu() {
+
+        boolean borrowing = true;
+
+        while (borrowing) {
+
+            System.out.println("--- Borrower menu ---");
+            System.out.println("1. Borrow book");
+            System.out.println("2. Return book");
+            System.out.println("3. Show loaned books");
+            System.out.println("4. Show all books");
+            System.out.println("5. Return to main menu");
+            System.out.println("---------------------");
+
+            int option = Integer.parseInt(scanner.nextLine());
+
+            //if to control input
+
+            switch (option) {
+                case 1:
+                    System.out.println("Title of book");
+                    //findBook, check if available.
+                    //borrowBook (add book to list of borrower.
+                    break;
+                case 2:
+                    System.out.println("Which book to return: ");
+                    //findLoanedBookBorrower, search list and add to Library list.
+                    break;
+                case 3:
+                    System.out.println("Show loaned books");
+                    //showBooks in borrower array
+                    break;
+                case 4:
+                    System.out.println("Show all books in library");
+                    break;
+                case 5:
+                    borrowing = false;
+                    break;
+                default:
+                    System.out.println("Enter a number between 1 - 4");
+                    break;
+            }
+        }
+
     }
 
     private void showAllBooks() {
         for (Book book : books) {
             book.getInfo();
         }
+    }
+
+    private void verifyUser() {
+        System.out.println("Enter name: ");
+        //Search for user
+        System.out.println("Enter password: ");
+        //Check that password is correct?
+        System.out.println("Wrong password, try again.");
     }
 }
